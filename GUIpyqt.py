de68fb555ba1627be6e0f4118a90c37f8ac5762e
Kinddle <h88888888123@163.com>
2020-09-27 21:35:05 +0800

    PyQt 窗口完成

--- GUIpyqt.py
+++ GUIpyqt.py
@@ -17,79 +17,101 @@ __author__ = 'Kinddle'
 from PyQt5 import QtCore, QtGui, QtWidgets
 import sys
 import qtawesome
+import pyqtgraph as pg
 
 
 class MainUi(QtWidgets.QMainWindow):
-    def __init__(self):
+    def __init__(self, datalink):
         super().__init__()
+        self.datadic = datalink
+        self.title = 'Contoller'
+        self.left = 100
+        self.top = 100
+        self.width = 960
+        self.height = 700
         self.init_ui()
 
     def init_ui(self):
-        self.setFixedSize(960, 700)
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left, self.top, self.width, self.height)
+
         self.main_widget = QtWidgets.QWidget()  # 创建窗口主部件
         self.main_layout = QtWidgets.QGridLayout()  # 创建主部件的网格布局
         self.main_widget.setLayout(self.main_layout)  # 设置窗口主部件布局为网格布局
 
         self.left_widget = QtWidgets.QWidget()  # 创建左侧部件
-        self.left_widget.setObjectName('left_widget')
         self.left_layout = QtWidgets.QGridLayout()  # 创建左侧部件的网格布局层
         self.left_widget.setLayout(self.left_layout)  # 设置左侧部件布局为网格
 
         self.right_widget = QtWidgets.QWidget()  # 创建右侧部件
-        self.right_widget.setObjectName('right_widget')
         self.right_layout = QtWidgets.QGridLayout()
         self.right_widget.setLayout(self.right_layout)  # 设置右侧部件布局为网格
 
-        self.main_layout.addWidget(self.left_widget, 0, 0, 12, 2)  # 左侧部件在第0行第0列，占8行3列
-        self.main_layout.addWidget(self.right_widget, 0, 2, 12, 10)  # 右侧部件在第0行第3列，占8行9列
+
+        self.control_widget = QtWidgets.QWidget() #创建控制部件
+        self.control_layout = QtWidgets.QGridLayout()
+        self.control_widget.setLayout(self.control_layout)  # 设置控制部件布局为网格
+
+        self.main_layout.addWidget(self.left_widget, 0, 0, 13, 2)  # 左侧部件在第0行第0列，占8行3列
+        self.main_layout.addWidget(self.right_widget, 0, 2, 12, 6)  # 右侧部件在第0行第3列，占8行9列
+
+        self.main_layout.setColumnStretch(0, 2)
+        self.main_layout.setColumnStretch(1, 6)
         self.setCentralWidget(self.main_widget)  # 设置窗口主部件
 
-        self.left_close = QtWidgets.QPushButton("")  # 关闭按钮
-        self.left_visit = QtWidgets.QPushButton("")  # 空白按钮
-        self.left_mini = QtWidgets.QPushButton("")  # 最小化按钮
+        self.lb1 = QtWidgets.QLabel("呼吸速率:0")
+        self.lb2 = QtWidgets.QLabel("心跳速率:0")
+        self.lb3 = QtWidgets.QLabel("跌倒检测：")
+        self.lb4 = QtWidgets.QLabel('呼吸心跳雷达：')
+        self.lb5 = QtWidgets.QLabel('航迹跌倒雷达：')
+        self.lb6 = QtWidgets.QLabel('目标检测与跟踪')
+        self.txt = QtWidgets.QTextEdit()
 
-        self.left_label_1 = QtWidgets.QPushButton("每日推荐")
-        self.left_label_1.setObjectName('left_label')
-        self.left_label_2 = QtWidgets.QPushButton("我的音乐")
-        self.left_label_2.setObjectName('left_label')
-        self.left_label_3 = QtWidgets.QPushButton("联系与帮助")
-        self.left_label_3.setObjectName('left_label')
+        self.plot_Track = pg.PlotWidget()
+        self.plot_Rate_breath = pg.PlotWidget()
+        self.plot_Rate_heart = pg.PlotWidget()
 
-        self.left_button_1 = QtWidgets.QPushButton(qtawesome.icon('fa.music', color='white'), "华语流行")
-        self.left_button_1.setObjectName('left_button')
-        self.left_button_2 = QtWidgets.QPushButton(qtawesome.icon('fa.sellsy', color='white'), "在线FM")
-        self.left_button_2.setObjectName('left_button')
-        self.left_button_3 = QtWidgets.QPushButton(qtawesome.icon('fa.film', color='white'), "热门MV")
-        self.left_button_3.setObjectName('left_button')
-        self.left_button_4 = QtWidgets.QPushButton(qtawesome.icon('fa.home', color='white'), "本地音乐")
-        self.left_button_4.setObjectName('left_button')
-        self.left_button_5 = QtWidgets.QPushButton(qtawesome.icon('fa.download', color='white'), "下载管理")
-        self.left_button_5.setObjectName('left_button')
-        self.left_button_6 = QtWidgets.QPushButton(qtawesome.icon('fa.heart', color='white'), "我的收藏")
-        self.left_button_6.setObjectName('left_button')
-        self.left_button_7 = QtWidgets.QPushButton(qtawesome.icon('fa.comment', color='white'), "反馈建议")
-        self.left_button_7.setObjectName('left_button')
-        self.left_button_8 = QtWidgets.QPushButton(qtawesome.icon('fa.star', color='white'), "关注我们")
-        self.left_button_8.setObjectName('left_button')
-        self.left_button_9 = QtWidgets.QPushButton(qtawesome.icon('fa.question', color='white'), "遇到问题")
-        self.left_button_9.setObjectName('left_button')
-        self.left_xxx = QtWidgets.QPushButton(" ")
+        btn1 = QtWidgets.QPushButton("R up")
+        btn1.clicked.connect(lambda : print(1))
+        btn2 = QtWidgets.QPushButton('R down')
+        btn2.clicked.connect(lambda : print(1))
+        btn3 = QtWidgets.QPushButton('FT up')
+        btn3.clicked.connect(lambda : print(1))
+        btn4 = QtWidgets.QPushButton('FT down')
+        btn4.clicked.connect(lambda : print(1))
 
-        self.left_layout.addWidget(self.left_mini, 0, 0, 1, 1)
-        self.left_layout.addWidget(self.left_close, 0, 2, 1, 1)
-        self.left_layout.addWidget(self.left_visit, 0, 1, 1, 1)
-        self.left_layout.addWidget(self.left_label_1, 1, 0, 1, 3)
-        self.left_layout.addWidget(self.left_button_1, 2, 0, 1, 3)
-        self.left_layout.addWidget(self.left_button_2, 3, 0, 1, 3)
-        self.left_layout.addWidget(self.left_button_3, 4, 0, 1, 3)
-        self.left_layout.addWidget(self.left_label_2, 5, 0, 1, 3)
-        self.left_layout.addWidget(self.left_button_4, 6, 0, 1, 3)
-        self.left_layout.addWidget(self.left_button_5, 7, 0, 1, 3)
-        self.left_layout.addWidget(self.left_button_6, 8, 0, 1, 3)
-        self.left_layout.addWidget(self.left_label_3, 9, 0, 1, 3)
-        self.left_layout.addWidget(self.left_button_7, 10, 0, 1, 3)
-        self.left_layout.addWidget(self.left_button_8, 11, 0, 1, 3)
-        self.left_layout.addWidget(self.left_button_9, 12, 0, 1, 3)
+
+        #布局
+        self.left_layout.addWidget(self.lb1, 0, 0, 1, 2)
+        self.left_layout.addWidget(self.plot_Rate_breath, 1, 0, 3, 2)
+        self.left_layout.addWidget(self.lb2, 4, 0, 1, 2)
+        self.left_layout.addWidget(self.plot_Rate_heart, 5, 0, 3, 2)
+        self.left_layout.addWidget(self.lb3, 8, 0 ,1, 2)
+        self.left_layout.addWidget(self.txt, 9, 0, 4, 2)
+        self.left_layout.setRowStretch(0, 1)
+        self.left_layout.setRowStretch(1, 4)
+        self.left_layout.setRowStretch(4, 1)
+        self.left_layout.setRowStretch(5, 4)
+        self.left_layout.setRowStretch(8, 1)
+        self.left_layout.setRowStretch(9, 4)
+
+        self.right_layout.addWidget(self.lb6, 0, 0, 1, 6)
+        self.right_layout.addWidget(self.plot_Track, 1, 0, 10, 6)
+        self.right_layout.addWidget(self.control_widget, 11, 0, 1, 6)
+        self.right_layout.setRowStretch(1, 0)
+        self.right_layout.setRowStretch(2, 5)
+        self.right_layout.setRowStretch(11,0)
+
+        self.control_layout.addWidget(self.lb4,0,0,1,2)
+        self.control_layout.addWidget(self.lb5,0,2,1,2)
+        self.control_layout.addWidget(btn1, 1, 0)
+        self.control_layout.addWidget(btn2, 1, 1)
+        self.control_layout.addWidget(btn3, 1, 2)
+        self.control_layout.addWidget(btn4, 1, 3)
+        self.right_layout.setRowStretch(0, 1)
+        self.right_layout.setRowStretch(1, 1)
+
+
 
 def main():
     app = QtWidgets.QApplication(sys.argv)
